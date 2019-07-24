bd4ff8614c8df52f71f1a075d395242c8349e6b9
lizhehao <lizhehao@eidos.ic.i.u-tokyo.ac.jp>
2019-07-24 14:22:22 +0900

    Word splitting

--- original_data_doc2vec/split_words.py
+++ original_data_doc2vec/split_words.py
@@ -7,9 +7,9 @@ from janome.tokenfilter import ExtractAttributeFilter, POSStopFilter
 t = Tokenizer(mmap=True)
 
 # An analyzer need 3 parameters, which are: char_filters, tokenizer, token_filters
-a = Analyzer([UnicodeNormalizeCharFilter], t, [POSStopFilter(['記号']), ExtractAttributeFilter('base_form')])
+a = Analyzer([UnicodeNormalizeCharFilter], t, [POSStopFilter(['記号'])])
 
 def split_words(text):
-    for word in a.analyze(text):
+    for word in ExtractAttributeFilter('base_form').apply(a.analyze(text)):
         print(word)
     return 
